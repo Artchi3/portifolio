71f9a7fd16e84c6fd717b6930c1bbc83d7f3d625
Artchi3 <arturrsilva.ars@gmail.com>
2025-01-30 18:37:55 -0300

    feat: structures

--- artchi3/src/App.js
+++ artchi3/src/App.js
@@ -4,20 +4,67 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="greetings">
+          <h1>Olá Mundo</h1>
+          <h2>Me chame de <b>Artchi3</b>, <p>Desenvolvedor Front-End</p></h2>
+      </section>
+      <section class="aboutme">
+          <div class="aboutme-txt">
+              <h3>Quem Sou Eu?</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem magnam deleniti et voluptates. Dignissimos quod, commodi dolores nisi sapiente impedit porro nemo quos ducimus. Exercitationem error sapiente omnis at facilis.</p>    
+          </div>
+          <div class="aboutme-images">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk8QfWf1dVIl8fJcdaygt30RU8lpkHLYmlEA&s" alt=""/>
+              <img src="https://image-processor-storage.s3.us-west-2.amazonaws.com/images/3cf61c1011912a2173ea4dfa260f1108/halo-of-neon-ring-illuminated-in-the-stunning-landscape-of-yosemite.jpg" alt=""/>
+              <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" alt=""/>
+          </div>
+      </section>
+      <section class="projects">
+          <img src="costeladeadao" alt=""/> 
+      </section>
+      <section class="socials">
+          <div class="moreofme">
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a>
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a>
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a> 
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a>
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a>
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a> 
+
+          </div>
+          <div class="contactme">
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a>
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a>
+              <a href="">
+                  <img src="" alt=""/>
+                  @Artchi3
+              </a> 
+          </div>
+      </section> 
     </div>
   );
 }
---
+++ artchi3/src/models/Header.js
@@ -0,0 +1,17 @@
+ 
+import './App.css';
+
+function App() {
+  return ( 
+    <header className="App-header">
+        <p>Artchi3</p>
+        <ul className='menu'>
+            <li>Contato</li>
+            <li>Contato</li>
+            <li>SAP</li>
+        </ul>
+    </header> 
+  );
+}
+
+export default App;
